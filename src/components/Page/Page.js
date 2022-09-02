588a7a9ccb16952e20a8e3444520b4790fed0f6b
Vincent Zhang <vzhang@opentable.com>
2022-09-01 23:03:02 -0700

    Hide unfinished parts for now

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -13,11 +13,11 @@ function Page() {
             <Header/>
             <Hero/>
             <div className={styles.content}>
-                <Intro/>
-                <History/>
-                <Contact/>
+                {/* <Intro/> */}
+                {/* <History/> */}
+                {/* <Contact/> */}
             </div>
-            <Footer/>
+            {/* <Footer/> */}
         </div>
     );
 }
