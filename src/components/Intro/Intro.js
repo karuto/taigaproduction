93aaed657d28fd22a654f6435c966837dd0d8622
Vincent Zhang <vzhang@opentable.com>
2022-09-02 21:26:21 -0700

    Updated styling for intro

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,34 +4,16 @@ import * as Scroll from 'react-scroll';
 import { Link } from 'react-scroll';
 import profileImage from '../../assets/taiga-gura.jpg';
 
-/*
-*/
 
 function Intro () {
 	return (
 		<div>
 		<section className={styles['container--intro']}>
-			<div className={styles['heading--primary']} style={{backgroundImage: `url('dist/${profileImage}')`}}>
-				{/* <h2 className={styles.headingtext}>People first,<br/> influencers second</h2> */}
-			</div>
-			<div className={styles['content']}>
-				{/* <div className={styles['profile-image']} style={{backgroundImage: `url('dist/${profileImage}')`}}>
-				</div> */}
-				{/* <p className={styles['intro--primaryhero']}>
-					Hi, I'm Taiga 👋
-				</p> */}
-				<p className={styles.p}>
-					<span className={styles.highlight}>Hi, I'm Taiga 👋</span>a professional cosplayer that has been active in the cosplay community for 12+ years.
-				</p>
-				<p className={styles.p}>
-					I founded Taiga Production to help my family of creator friends grow and excel.
-				</p>
-				<p className={styles.p}>
-					We are small by design.
-				</p>
-				<p className={styles.p}>
-					Because we geniunely care.
-				</p>
+			<div className={styles['heading--primary']} style={{backgroundImage: `url('dist/${profileImage}')`}}></div>
+			<div className={styles.content}>
+				<p className={styles.p}><span className={styles.highlight}>Hi, I'm Taiga 👋</span>a professional cosplayer that has been active in the cosplay community for 12+ years.</p>
+				<p className={styles.p}>I founded Taiga Production to help my family of creator friends grow and excel.</p>
+				<p className={styles.p}>We are small by design. Because we geniunely care.</p>
 			</div>
 			<div className={styles['action--primary']}>
 				{/* <Link activeClass="active" to="history" spy={true} smooth={true} offset={-50} duration={500} className={styles['link--primary']}> */}
