05def94448f7da5b1e17e0c9d9096825e1ae17ff
Vincent Zhang <vzhang@opentable.com>
2023-04-23 15:20:38 -0700

    Updated brands

--- src/components/Brands/Brands.js
+++ src/components/Brands/Brands.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import styles from './Brands.css';
-import logoGenshin from '../../assets/logo-genshin.png';
+import logoNikke from '../../assets/logo-nikke.png';
 import logoYostar from '../../assets/logo-yostar.png';
 import logoArknights from '../../assets/logo-arknights.png';
 import logoAzurLane from '../../assets/logo-azur-lane.png';
-import logoHonkai3rd from '../../assets/logo-honkai-3rd.png';
-import logoHonkaiStarRail from '../../assets/logo-honkai-star-rail.png';
+import logoLevelInfinite from '../../assets/logo-level-infinite.png';
+import logoPaper from '../../assets/logo-paper.png';
 
 function Brands () {
     return (
 		<section className={styles.container}>
 			<h2 className={styles.title}>Brands we worked with</h2>
 			<div className={styles.brands}>
-				<img className={styles.brand} src={`dist/${logoGenshin}`} />
+				<img className={styles.brand} src={`dist/${logoNikke}`} />
 				<img className={styles.brand} src={`dist/${logoYostar}`} />
 				<img className={styles.brand} src={`dist/${logoAzurLane}`} />
 				<img className={styles.brand} src={`dist/${logoArknights}`} />
-				<img className={styles.brand} src={`dist/${logoHonkai3rd}`} />
-				<img className={styles.brand} src={`dist/${logoHonkaiStarRail}`} />
+				<img className={styles.brand} src={`dist/${logoLevelInfinite}`} />
+				<img className={styles.brand} src={`dist/${logoPaper}`} />
 			</div>
 		</section>
     );
